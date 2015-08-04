07bfb018fcac026c30d76aa54dd39eb269b621d2
Adam Stokes <adam.stokes@ubuntu.com>
2015-08-04 11:27:03 -0400

    update
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var config = {
+    "title": "Adam Stokes",
+    "description": "Bumblebee Tuna",
+    "baseUrl": "http://astokes.org/",
+    port: 8080,
+    secret: "p00b34r"
+};
+
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,87 @@
+"use strict";
+var Hapi = require("hapi");
+var Good = require("good");
+var config = require("./config");
+var server = new Hapi.Server();
+var Yar = require("yar");
+
+server.connection({
+    port: config.port
+});
+
+var yarOpts = {
+    cookieOptions: {
+        password: config.secret,
+        clearInvalid: true,
+        isSecure: false
+    }
+};
+
+server.register({
+    register: Yar,
+    options: yarOpts
+}, function(err) {
+    if (err) {
+        throw Error(err);
+    }
+});
+
+server.views({
+    engines: {
+        jade: require("jade")
+    },
+    relativeTo: __dirname,
+    path: "./templates",
+    context: {
+        site: config.info
+    }
+});
+
+server.route({
+    path: "/favicon.ico",
+    method: "GET",
+    handler: {
+        file: "./favicon.ico"
+    }
+});
+
+server.route({
+    path: "/",
+    method: "GET",
+    handler: function(request, reply) {
+        return reply.view("index");
+    }
+});
+
+server.route({
+    path: "/static/{path*}",
+    method: "GET",
+    handler: {
+        directory: {
+            path: "./public",
+            listing: false,
+            index: false
+        }
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require("good-console"),
+            events: {
+                response: "*",
+                log: "*"
+            }
+        }]
+    }
+}, function(err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function() {
+        server.log("info", "Server running at: " + server.info.uri);
+    });
+});
